da50dc37e9b2247f6aef8875b292c44f515936c5
Andy Cheng <ccjeng@gmail.com>
2015-07-10 13:30:40 +0800

    no message

--- app/src/main/java/com/oddsoft/buddhavoice2/VoiceListener.java
+++ app/src/main/java/com/oddsoft/buddhavoice2/VoiceListener.java
@@ -53,14 +53,6 @@ public class VoiceListener extends Activity {
 
     private AdView adView;
 
-    private DrawerLayout mDrawerLayout;
-    private ActionBarDrawerToggle mDrawerToggle;
-    private LinearLayout mLlvDrawerContent;
-    private ListView mLsvDrawerMenu;
-
-    // 記錄被選擇的選單指標用
-    private int mCurrentMenuItemPosition = -1;
-
     final Handler updateHandler = new Handler();
 
     @Override
@@ -68,14 +60,11 @@ public class VoiceListener extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.listener);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
 
         Analytics ga = new Analytics();
         if (!BuddhaVoice.APPDEBUG)
             ga.initTracker(this);
-
-        initActionBar();
-        initDrawer();
-        initDrawerList();
 
         //get intent values
         Bundle bunde = this.getIntent().getExtras();
@@ -121,124 +110,6 @@ public class VoiceListener extends Activity {
     }
 
 
-    private void initActionBar(){
-        //顯示 Up Button (位在 Logo 左手邊的按鈕圖示)
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-        //打開 Up Button 的點擊功能
-        getActionBar().setHomeButtonEnabled(true);
-    }
-
-    private void initDrawer() {
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drw_layout1);
-        // 設定 Drawer 的影子
-        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
-
-        mDrawerToggle = new ActionBarDrawerToggle(
-                this,
-                mDrawerLayout,    // 讓 Drawer Toggle 知道母體介面是誰
-                R.drawable.ic_drawer, // Drawer 的 Icon
-                R.string.app_name, // Drawer 被打開時的描述
-                R.string.app_name // Drawer 被關閉時的描述
-        ) {
-            //被打開後要做的事情
-            @Override
-            public void onDrawerOpened(View drawerView) {
-                // 將 Title 設定為自定義的文字
-                getActionBar().setTitle(R.string.app_name);
-            }
-
-            //被關上後要做的事情
-            @Override
-            public void onDrawerClosed(View drawerView) {
-                // 將 Title 設定回 APP 的名稱
-                getActionBar().setTitle(R.string.app_name);
-            }
-        };
-
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
-
-    }
-
-    private void initDrawerList() {
-
-        String[] drawer_menu = this.getResources().getStringArray(R.array.drawer_menu);
-
-        // 定義新宣告的兩個物件：選項清單的 ListView 以及 Drawer內容的 LinearLayou
-        mLsvDrawerMenu = (ListView) findViewById(R.id.lsv_drawer_menu);
-        mLlvDrawerContent = (LinearLayout) findViewById(R.id.llv_left_drawer);
-
-        int[] iconImage = { android.R.drawable.ic_menu_preferences, android.R.drawable.ic_dialog_info };
-
-        List<HashMap<String,String>> lstData = new ArrayList<HashMap<String,String>>();
-        for (int i = 0; i < iconImage.length; i++) {
-            HashMap<String, String> mapValue = new HashMap<String, String>();
-            mapValue.put("icon", Integer.toString(iconImage[i]));
-            mapValue.put("title", drawer_menu[i]);
-            lstData.add(mapValue);
-        }
-
-        SimpleAdapter adapter = new SimpleAdapter(this, lstData
-                , R.layout.drawer_item
-                , new String[]{"icon", "title"}
-                , new int[]{R.id.rowIcon, R.id.rowText});
-        mLsvDrawerMenu.setAdapter(adapter);
-
-        // 當清單選項的子物件被點擊時要做的動作
-        mLsvDrawerMenu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                selectMenuItem(position);
-            }
-        });
-
-    }
-
-    private void selectMenuItem(int position) {
-        mCurrentMenuItemPosition = position;
-
-        switch (mCurrentMenuItemPosition) {
-            case 0:
-                startActivity(new Intent(this, Prefs.class));
-                break;
-            case 1:
-                //show about message
-                new AlertDialog.Builder(this)
-                        .setTitle(R.string.about_title)
-                        .setMessage(R.string.about_text)
-                        .setPositiveButton(R.string.ok_label,
-                                new DialogInterface.OnClickListener(){
-                                    public void onClick(
-                                            DialogInterface dialoginterface, int i){
-                                        //empty
-                                    }
-                                })
-                        .show();
-                break;
-        }
-
-        // 將選單的子物件設定為被選擇的狀態
-        mLsvDrawerMenu.setItemChecked(position, true);
-
-        // 關掉 Drawer
-        mDrawerLayout.closeDrawer(mLlvDrawerContent);
-    }
-
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        mDrawerToggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        mDrawerToggle.onConfigurationChanged(newConfig);
-    }
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -252,11 +123,10 @@ public class VoiceListener extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
 
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-
         switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
             case R.id.stop_button:
                 stopMusic();
                 //back to main
