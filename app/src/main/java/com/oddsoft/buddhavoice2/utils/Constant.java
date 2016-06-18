5af8b840219b7c302520f665636724a743a09037
andycheng <ccjeng@gmail.com>
2016-06-18 22:18:55 +0800

    no message

---
+++ app/src/main/java/com/oddsoft/buddhavoice2/utils/Constant.java
@@ -0,0 +1,14 @@
+package com.oddsoft.buddhavoice2.utils;
+
+import com.oddsoft.buddhavoice2.BuildConfig;
+
+/**
+ * Created by andycheng on 2016/2/24.
+ */
+public class Constant {
+
+    public static final String ADMob_TestDeviceID = "3C8B080F4F918474804E2685BA46E2B3";
+    public static final String ADMob_BuddhaVoice = BuildConfig.ADMob_BuddhaVoice;
+    public static final String ADMob_BuddhaVoiceMain = BuildConfig.ADMob_BuddhaVoiceMain;
+
+}
